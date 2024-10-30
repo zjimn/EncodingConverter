8776a0b0017357342a4c970de329d0f0b4a3bf36
jim <createzn@gmail.com>
2024-10-30 13:46:45 +0800

    refine

--- util/convert_po_to_mo.py
+++ util/convert_po_to_mo.py
@@ -12,21 +12,17 @@ def convert_po_to_mo():
                 po_file_path = os.path.join(dirpath, filename)
                 mo_file_path = os.path.splitext(po_file_path)[0] + '.mo'  # Change the file extension to .mo
 
-                # 检查 .po 文件是否存在
                 if not os.path.exists(po_file_path):
                     print(f"PO file does not exist: {po_file_path}")
                     continue
 
-                # 检查 .mo 文件是否存在
                 if os.path.exists(mo_file_path):
-                    # 比较文件的修改时间
                     po_mtime = os.path.getmtime(po_file_path)
                     mo_mtime = os.path.getmtime(mo_file_path)
                     if po_mtime <= mo_mtime:
                         print(f"MO file is up to date. No conversion needed for: {mo_file_path}")
-                        continue  # 如果 .mo 文件是最新的，则跳过转换
+                        continue
 
-                # 加载 .po 文件并保存为 .mo 文件
                 po = polib.pofile(po_file_path)
                 po.save_as_mofile(mo_file_path)
                 print(f"Converted {po_file_path} to {mo_file_path}")

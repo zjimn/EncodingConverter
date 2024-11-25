fbcde3c87afd375364ce6ff90ac44d35750cf4b1
jim <455428781@qq.com>
2024-11-25 16:00:36 +0800

    modify get project path

--- util/path_util.py
+++ util/path_util.py
@@ -6,4 +6,5 @@ def get_base_path():
     if getattr(sys, 'frozen', False):
         return os.path.dirname(sys.executable)
     else:
-        return os.path.abspath(os.path.dirname(__file__))
+        return os.getcwd()
+        #return os.path.abspath(os.path.dirname(__file__))
